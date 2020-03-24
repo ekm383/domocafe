331f0b9ddc99ad6b059350a2189dd91985695c42
Eric Manongdo <eric@superscriptcreative.com>
2020-03-24 13:18:12 -1000

    Start

---
+++ src/components/globals/footer/Footer.js
@@ -0,0 +1,93 @@
+import React, { Component } from "react"
+import styled from "styled-components"
+import map from "../../../images/bg/domo-cafe-hawaii-map.jpg"
+import { FaInstagram, FaFacebook, FaGoogle } from "react-icons/fa"
+
+class Footer extends Component {
+  state = {
+    icons: [
+      {
+        id: 2,
+        icon: <FaInstagram className="icon facebook-instagram" />,
+        path: "https://www.instagram.com/restaurantmarketinghawaii",
+      },
+      {
+        id: 3,
+        icon: <FaGoogle className="icon google-icon" />,
+        path: "https://www.google.com/restaurantmarketinghawaii",
+      },
+      {
+        id: 1,
+        icon: <FaFacebook className="icon facebook-icon" />,
+        path: "https://www.facebook.com/restaurantmarketinghawaii",
+      },
+    ],
+  }
+  render() {
+    return (
+      <FooterWrapper>
+        <div className="box contact">
+          <h4>Contact Information</h4>
+          <p>
+            1016 Maunakea St,
+            <br />
+            Honolulu, HI 96817
+          </p>
+          <p className="phone">(808) 587-8338</p>
+          <p>Mon - Sat 9:00am - 6:00pm</p>
+          <p>Sun 10:00am - 3:00pm</p>
+          {this.state.icons.map(item => (
+            <a
+              href={item.path}
+              key={item.id}
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {item.icon}
+            </a>
+          ))}
+        </div>
+        <div className="box map">
+          <img src={map} alt="map" width="100%" height="100%" />
+        </div>
+      </FooterWrapper>
+    )
+  }
+}
+
+const FooterWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+  height: 100%;
+  background: var(--gray);
+  color: var(--white);
+  p,
+  .phone {
+    font-size: 0.8rem;
+  }
+  .box {
+    flex-basis: 46%;
+    padding: 2rem;
+  }
+  .map {
+    background: lightgray;
+    padding: 0;
+  }
+  a {
+    color: var(--white);
+    font-size: 2rem;
+    line-height: 6rem;
+    margin-right: 5px;
+  }
+  @media (max-width: 768px) {
+    .box {
+      flex-basis: 100%;
+    }
+    .phone {
+      font-size: 0.8rem;
+    }
+  }
+`
+
+export default Footer
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,47 @@
 import React from "react"
-import { Link } from "gatsby"
-
 import Layout from "../components/layout"
-import Image from "../components/image"
+import AniLink from "gatsby-plugin-transition-link/AniLink"
 import SEO from "../components/seo"
+import HeaderIndex from "../components/globals/header/HeaderIndex"
+import Banner from "../components/globals/banner/Banner"
+import Button from "../components/globals/button/Button"
+import Section from "../components/globals/section/Section"
+import ContactForm from "../components/ContactForm"
+import imgIndex from "../images/bg/domo-cafe-hawaii-restaurant.jpg"
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <SEO
+      title="Home"
+      description="Domo Cafe is located on Maunakea Street in Downtown Honolulu. We are open daily serving the best ahi poke and sushi.`,
+      author: `@restaurantmarketinghawaii"
+      keywords={[
+        `Downtown Honolulu Restaurant`,
+        `Hawaii Restaurant`,
+        `Hawaii Local Food`,
+        `Honolulu Takeout`,
+        `Honolulu Poke`,
+        `Honolulu Sushi`,
+      ]}
+    />
+    <HeaderIndex img={imgIndex}>
+      <Section style={{ width: "100vw", alignItems: "center" }}>
+        <Banner
+          titleSmall="DOMO CAFE"
+          subtitle="Domo Cafe is located on Maunakea Street in Downtown Honolulu. We are open daily serving the best ahi poke and sushi."
+        >
+          <AniLink fade to="#menu">
+            <Button>ORDER TAKEOUT</Button>
+          </AniLink>
+        </Banner>
+      </Section>
+    </HeaderIndex>
+    <Section id="menu" style={{ height: "50vh" }}>
+      <div>menu section</div>
+    </Section>
+    <Section id="information" style={{ height: "50vh" }}>
+      <ContactForm />
+    </Section>
   </Layout>
 )
 
